ec5d01ec6d2bde50d96dc2beff651dbe032a7bdf
achobanov <alexchobanov@gmail.com>
2025-04-24 19:23:57 +0300

    Add AppCenter for iOS

--- src/Witness/EMS.Witness/Platforms/iOS/AppDelegate.cs
+++ src/Witness/EMS.Witness/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,20 @@
 ﻿using Foundation;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
 
 namespace EMS.Witness;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	private const string EMS_WITNESS_IOS = "8a64ff1f-469b-43c0-a4fd-dcfe6c4d8b20";
+
+	protected override MauiApp CreateMauiApp()
+	{
+		var secret = EMS_WITNESS_IOS;
+		AppCenter.Start(secret, typeof(Analytics), typeof(Crashes));
+
+		return MauiProgram.CreateMauiApp();
+	} 
 }
